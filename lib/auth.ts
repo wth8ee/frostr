feffd843bcd0c42d22c543e82fc03754e53a95dc
wth8ee <wth8eeyv2@gmail.com>
2026-06-10 22:36:13 +0300

    fixed base url

--- lib/auth.ts
+++ lib/auth.ts
@@ -8,10 +8,7 @@ const prisma = new PrismaClient();
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, { provider: "postgresql" }),
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? `https://${process.env.VERCEL_URL}`
-      : process.env.BETTER_AUTH_URL,
+  baseURL: process.env.BETTER_AUTH_URL,
   emailAndPassword: {
     enabled: true,
   },
